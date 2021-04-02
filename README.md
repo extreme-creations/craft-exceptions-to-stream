358b3223fad0b290bdb0311e5c76143754442dfa
Joe Pagan <joepagan@gmail.com>
2021-04-02 17:22:12 +0100

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+# Exceptions To Stream plugin for Craft CMS 3.x
+
+A small plugin to capture all thrown exceptions to send to standard error stream
+
+![Screenshot](resources/img/plugin-logo.png)
+
+## Requirements
+
+This plugin requires Craft CMS 3.0.0-beta.23 or later.
+
+## Installation
+
+To install the plugin, follow these instructions.
+
+1. Open your terminal and go to your Craft project:
+
+        cd /path/to/project
+
+2. Then tell Composer to load the plugin:
+
+        composer require extreme-creations/craft-exceptionstostream
+
+3. In the Control Panel, go to Settings → Plugins and click the “Install” button for Exceptions To Stream.
+
+## Exceptions To Stream Overview
+
+-Insert text here-
+
+## Configuring Exceptions To Stream
+
+-Insert text here-
+
+## Using Exceptions To Stream
+
+-Insert text here-
+
+## Exceptions To Stream Roadmap
+
+Some things to do, and ideas for potential features:
+
+* Release it
+
+Brought to you by [Joe Pagan](https://madebyextreme.com/)
---
+++ src/ExceptionsToStream.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Exceptions To Stream plugin for Craft CMS 3.x
+ *
+ * A small plugin to capture all thrown exceptions to send to standard error stream
+ *
+ * @link      https://madebyextreme.com/
+ * @copyright Copyright (c) 2021 Joe Pagan
+ */
+
+namespace extremecreations\exceptionstostream;
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Plugins;
+use craft\events\PluginEvent;
+
+use craft\events\ExceptionEvent;
+use craft\web\ErrorHandler;
+
+use yii\base\Event;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+/**
+ * Class ExceptionsToStream
+ *
+ * @author    Joe Pagan
+ * @package   ExceptionsToStream
+ * @since     1.0.0
+ *
+ */
+class ExceptionsToStream extends Plugin
+{
+    /**
+     * @var ExceptionsToStream
+     */
+    public static $plugin;
+
+    /**
+     * @var string
+     */
+    public $schemaVersion = '1.0.0';
+
+    /**
+     * @var bool
+     */
+    public $hasCpSettings = false;
+
+    /**
+     * @var bool
+     */
+    public $hasCpSection = false;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
+
+        Event::on(
+            ErrorHandler::className(),
+            ErrorHandler::EVENT_BEFORE_HANDLE_EXCEPTION,
+            function(ExceptionEvent $event) {
+                $logger = new Logger('app'));
+                $logger->pushHandler(new StreamHandler('php://stderr', \Monolog\Logger::WARNING));
+                $logger->critical($event->exception);
+            }
+      );
+
+        Craft::info(
+            Craft::t(
+                'exceptions-to-stream',
+                '{name} plugin loaded',
+                ['name' => $this->name]
+            ),
+            __METHOD__
+        );
+    }
+}
