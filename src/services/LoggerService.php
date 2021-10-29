cf18f67b4f28091e19af4003d02b8417bfbf7085
Joe Pagan <2337910+joepagan@users.noreply.github.com>
2021-10-29 17:18:03 +0100

    Updating readme to match current logic Updating composer version Updating service shorthand name for less typing Updating main log service to now use multiple methods for different logging levels Updating handleException method with re-usable code

--- src/services/LoggerService.php
+++ src/services/LoggerService.php
@@ -10,65 +10,145 @@ use Monolog\Formatter\LineFormatter;
 
 class LoggerService extends Component
 {
+    private $logger;
 
-  /**
-   * Handle an exception and send to error stream
-   *
-   * @param [type] $exception
-   * @return void
-   */
-  public function handleException($exception)
-  {
-      // Get status code
-      $statusCode = $exception->statusCode ?? null;
+    private function setupLogger($message, $stream)
+    {
+        // Create a Logger
+        $this->logger = new Logger('app');
+        // Create a LineFormatter
+        $formatter = new LineFormatter();
+        // Allow inline line breaks
+        $formatter->allowInlineLineBreaks();
+        // Include stack traces
+        $formatter->includeStacktraces();
+        // Set the formatter for the stream
+        $stream->setFormatter($formatter);
+        // Push the stream to logger
+        $this->logger->pushHandler($stream);
+    }
+    /**
+     * debug log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function debug(string $message)
+    {
+        $stream = new StreamHandler('php://stdout' , Logger::DEBUG);
+        $this->setupLogger($message, $stream);
+        $this->logger->debug($message);
+    }
 
-      // Check if we should skip status code
-      if (preg_match('/4[0-9][0-9]/', $statusCode)) {
-          return;
-      }
+    /**
+     * info log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function info(string $message)
+    {
+        $stream = new StreamHandler('php://stdout' , Logger::INFO);
+        $this->setupLogger($message, $stream);
+        $this->logger->info($message);
+    }
 
-      // Create a Logger
-      $logger = new Logger('app');
-      // Create a LineFormatter
-      $formatter = new LineFormatter();
-      // Allow inline line breaks
-      $formatter->allowInlineLineBreaks();
-      // Include stack traces
-      $formatter->includeStacktraces();
-      // Create a stream
-      $stream = new StreamHandler('php://stderr', Logger::ERROR);
-      // Set the formatter for the stream
-      $stream->setFormatter($formatter);
-      // Push the stream to logger
-      $logger->pushHandler($stream);
-      // Send all exceptions to logger
-      $logger->error($exception);
-  }
+    /**
+     * notice log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function notice(string $message)
+    {
+        $stream = new StreamHandler('php://stdout' , Logger::NOTICE);
+        $this->setupLogger($message, $stream);
+        $this->logger->notice($message);
+    }
 
-  /**
-   * Send a string of any time to the output stream
-   *
-   * @param string $message
-   * @return void
-   */
-  public function log(string $message)
-  {
-      // Create a Logger
-      $logger = new Logger('app');
-      // Create a LineFormatter
-      $formatter = new LineFormatter();
-      // Allow inline line breaks
-      $formatter->allowInlineLineBreaks();
-      // Include stack traces
-      $formatter->includeStacktraces();
-      // Create a stream
-      $stream = new StreamHandler('php://stdout', Logger::INFO);
-      // Set the formatter for the stream
-      $stream->setFormatter($formatter);
-      // Push the stream to logger
-      $logger->pushHandler($stream);
-      // Send all exceptions to logger
-      $logger->info($message);
-  }
+    /**
+     * warning log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function warning(string $message)
+    {
+        $stream = new StreamHandler('php://stdout' , Logger::WARNING);
+        $this->setupLogger($message, $stream);
+        $this->logger->warning($message);
+    }
+
+    /**
+     * error log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function error(string $message)
+    {
+        $stream = new StreamHandler('php://stderr' , Logger::ERROR);
+        $this->setupLogger($message, $stream);
+        $this->logger->error($message);
+    }
+
+    /**
+     * critical log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function critical(string $message)
+    {
+        $stream = new StreamHandler('php://stderr' , Logger::CRITICAL);
+        $this->setupLogger($message, $stream);
+        $this->logger->critical($message);
+    }
+
+    /**
+     * alert log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function alert(string $message)
+    {
+        $stream = new StreamHandler('php://stderr' , Logger::ALERT);
+        $this->setupLogger($message, $stream);
+        $this->logger->alert($message);
+    }
+
+    /**
+     * alert log
+     *
+     * @param string $message
+     * @return void
+     */
+    public function emergency(string $message)
+    {
+        $stream = new StreamHandler('php://stderr' , Logger::EMERGENCY);
+        $this->setupLogger($message, $stream);
+        $this->logger->emergency($message);
+    }
+
+    /**
+     * Handle an exception and send to error stream
+     *
+     * @param [type] $exception
+     * @return void
+     */
+    public function handleException($exception)
+    {
+        // Get status code
+        $statusCode = $exception->statusCode ?? null;
+
+        // Check if we should skip status code
+        if (preg_match('/4[0-9][0-9]/', $statusCode)) {
+            return;
+        }
+
+        // Send all exceptions to logger
+        $this->critical($exception);
+    }
 
 }
